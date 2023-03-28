041b3b65cf7ff547a01994ccafe0ffc38586de2e
ladorayn <lado.rayhan@ui.ac.id>
2023-03-28 16:18:57 +0700

    Initial Commit

---
+++ MarvelAPI/MarvelAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAPIApp.swift
+//  MarvelAPI
+//
+//  Created by Lado Rayhan on 28/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MarvelAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
